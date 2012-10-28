2f5d6d9189a0e9f75237336f20207dfebff9a663
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2012-10-28 15:31:44 -0500

    Research: Slowly adding things to SimpleLine reading python script. Other scripts will be based on a similar style

--- Research/Processing/Lines/SimpleLine.py
+++ Research/Processing/Lines/SimpleLine.py
@@ -2,9 +2,9 @@
 
 import os, sys
 import Image
+import math
 
 def main():
-    print "Hello"
     readSimpleImage()
 
 def readSimpleImage():
@@ -12,7 +12,31 @@ def readSimpleImage():
     if os.path.isfile(imageFilename):
         print "Reading Simple Image"
         
-        
+        img = Image.open(imageFilename)
+
+        print "Image Size: %s " % (img.size,)
+
+        xlen = img.size[0]
+        ylen = img.size[1]
+
+        pix = img.load()
+
+        foundblack = False
+
+        ii = 0
+        jj = 0
+
+        while (ii < xlen and foundblack == False):
+            jj = 0
+            while (jj < ylen and foundblack == False):
+                px = pix[ii, jj]
+                if (px[0]*px[0] + px[1]*px[1] + px[2]*px[2] < 10):
+                    foundblack = True
+                jj = jj + 1
+            ii = ii + 1
+
+        print ii
+        print jj
 
     else:
         print "File not found!"
