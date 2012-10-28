7308650261c1e60d938e2267a62464dc9ef95ed5
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2012-10-28 14:38:11 -0500

    Starting to add python scripts to try out extraction techniques etc.

---
+++ Research/Processing/Lines/SimpleLine.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os, sys
+import Image
+
+def main():
+	print "Hello"
+
+if __name__ == "__main__":
+	main()
+
+def readSimpleImage():
+    print "Reading Simple Image"
