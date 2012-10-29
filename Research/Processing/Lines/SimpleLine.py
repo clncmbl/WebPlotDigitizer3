129b8f8126b86a2766bb20328b919bab56b929aa
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2012-10-28 21:02:33 -0500

    Research: Using pylab to recover data points to analyze. For a simple line, just reduce this to a thin curve.

--- Research/Processing/Lines/SimpleLine.py
+++ Research/Processing/Lines/SimpleLine.py
@@ -3,6 +3,7 @@
 import os, sys
 import Image
 import math
+import pylab
 
 def main():
     readSimpleImage()
@@ -26,17 +27,22 @@ def readSimpleImage():
         ii = 0
         jj = 0
 
-        while (ii < xlen and foundblack == False):
+        datax = []
+        datay = []
+
+
+        while (ii < xlen):
             jj = 0
-            while (jj < ylen and foundblack == False):
+            while (jj < ylen):
                 px = pix[ii, jj]
                 if (px[0]*px[0] + px[1]*px[1] + px[2]*px[2] < 10):
-                    foundblack = True
+                    datax.append(ii)
+                    datay.append(ylen - jj)
                 jj = jj + 1
             ii = ii + 1
 
-        print ii
-        print jj
+        pylab.plot(datax, datay)
+        pylab.show()
 
     else:
         print "File not found!"
